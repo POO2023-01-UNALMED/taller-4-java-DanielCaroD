1ee341e8ff32982c52bb6ee913c008c3e2bf48b3
DanielCaroD <124928694+DanielCaroD@users.noreply.github.com>
2023-03-03 10:41:10 -0500

    .

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -4,9 +4,6 @@ public enum Tipo {
 		
 	DISCIPLINAR(10), FUNDAMENTACION(20), ELECTIVA(30);
 		
-	private int codigo;
-
-    private Tipo(int codigo) {
-        this.codigo = codigo;
+	private Tipo(int codigo) {
     }
 }
