4706539bb77eed690e035f194a6319117bbcd862
DanielCaroD <124928694+DanielCaroD@users.noreply.github.com>
2023-03-03 17:21:01 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -19,20 +19,17 @@ public class Persona {
     }
     
     public Persona(String nombre, long cedula) {
-        this.cedula = cedula;
-        this.nombre = nombre;
+    	this(cedula,nombre);
         totalPersonas++;
     }
     
     public Persona(long cedula) {
-        this.cedula = cedula;
-        this.nombre = "";
+    	this(cedula,"");
         totalPersonas++;
     }
 
     public Persona(String nombre) {
-    	this.cedula = ndcedula;
-        this.nombre = nombre;
+    	this(ndcedula,nombre);
         totalPersonas++;
         ndcedula++;
     }
